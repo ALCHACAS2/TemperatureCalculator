815fd44eb1d6641d5072765bd5e4abc653ccfb65
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-14 02:04:39 -0600

    Y a casi

--- Scrip.js
+++ Scrip.js
@@ -1,25 +1,39 @@
 function convertCelsiusToFahrenheit() {
-  celsius = document.getElementById("celsius").value;
-  /*--------°F = °C x 1.8 + 32---------*/
-  var fahrenheit = celsius * 1.8 + 32;
-  document.getElementById("resultado").textContent = fahrenheit;
+  grados = document.getElementById("grados").value;
+  var fahrenheit = grados * 1.8 + 32;
+  document.getElementById("resultado-fahrenheit").textContent = fahrenheit;
+  return fahrenheit;
 }
 function convertCelsiusToKelvin() {
-  /*----------K = °C + 273.15------------*/
+  grados = document.getElementById("grados").value;
+  var kelvin = parseFloat(grados) + 273.15;
+  document.getElementById("resultado-kelvin").textContent = kelvin;
 }
 function convertFahrenheitToCelsius() {
-  /*----------°C = (°F - 32) / 1.8-------*/
+  grados = document.getElementById("grados").value;
+  var celsius = (grados - 32) / 1.8;
+  document.getElementById("resultado").textContent = celsius;
+  return celsius;
 }
 function convertFahrenheitToKelvin() {
-  /*------°C = (°F - 32) / 1.8---K = °C + 273.15--*/
-  /*------°C = ((°F - 32) / 1.8)+273.15--*/
+  var celsius = convertFahrenheitToCelsius();
+  var kelvin = celsius + 273.15;
+  document.getElementById("resultado").textContent = kelvin;
 }
 function convertKelvinToFahrenheit() {
-  /*-------°C = K - 273.15---°F = °C x 1.8 + 32----*/
+  var celsius = kelvinToCelsius();
+  var fahrenheit = celsiusToFahrenheit(celsius);
+  document.getElementById("resultado").textContent = fahrenheit;
 }
 function convertKelvinToCelsius() {
-  /*------------°C = K - 273.15----------*/
+  grados = document.getElementById("grados").value;
+  var celsius = parseFloat(grados) - 273.15;
+  document.getElementById("resultado").textContent = celsius;
 }
-
-/*-----------RESULTADOS-------------------*/
-document.getElementById("resultado").textContent = convertCelsiusToFahrenheit();
+function kelvinToCelsius() {
+  grados = document.getElementById("grados").value;
+  return parseFloat(grados) - 273.15;
+}
+function celsiusToFahrenheit(celsius) {
+  return celsius * 1.8 + 32;
+}
