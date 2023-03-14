1ef89c816603a0091c8db7a416aceda2551e8fe3
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-13 20:21:08 -0600

    actru

---
+++ Scrip.js
@@ -0,0 +1,25 @@
+function convertCelsiusToFahrenheit() {
+    celsius = document.getElementById("celsius").value;
+  /*--------°F = °C x 1.8 + 32---------*/
+  var fahrenheit = celsius * 1.8 + 32;
+  return fahrenheit;
+}
+function convertCelsiusToKelvin() {
+  /*----------K = °C + 273.15------------*/
+}
+function convertFahrenheitToCelsius() {
+  /*----------°C = (°F - 32) / 1.8-------*/
+}
+function convertFahrenheitToKelvin() {
+  /*------°C = (°F - 32) / 1.8---K = °C + 273.15--*/
+  /*------°C = ((°F - 32) / 1.8)+273.15--*/
+}
+function convertKelvinToFahrenheit() {
+  /*-------°C = K - 273.15---°F = °C x 1.8 + 32----*/
+}
+function convertKelvinToCelsius() {
+  /*------------°C = K - 273.15----------*/
+}
+
+/*-----------RESULTADOS-------------------*/
+document.getElementById("resultado").textContent = convertCelsiusToFahrenheit();
