47f2bc2bf68630c7487735f41be2d2f8abec945d
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-15 01:28:37 -0600

    tdo funci0ona las converciones

--- Scrip.js
+++ Scrip.js
@@ -1,34 +1,32 @@
 function convertCelsiusToFahrenheit() {
   grados = document.getElementById("grados").value;
   var fahrenheit = grados * 1.8 + 32;
-  document.getElementById("resultado-fahrenheit").textContent = fahrenheit;
   return fahrenheit;
 }
 function convertCelsiusToKelvin() {
   grados = document.getElementById("grados").value;
   var kelvin = parseFloat(grados) + 273.15;
-  document.getElementById("resultado-kelvin").textContent = kelvin;
+  return kelvin;
 }
 function convertFahrenheitToCelsius() {
   grados = document.getElementById("grados").value;
   var celsius = (grados - 32) / 1.8;
-  document.getElementById("resultado").textContent = celsius;
   return celsius;
 }
 function convertFahrenheitToKelvin() {
   var celsius = convertFahrenheitToCelsius();
   var kelvin = celsius + 273.15;
-  document.getElementById("resultado").textContent = kelvin;
+  return kelvin;
 }
 function convertKelvinToFahrenheit() {
   var celsius = kelvinToCelsius();
   var fahrenheit = celsiusToFahrenheit(celsius);
-  document.getElementById("resultado").textContent = fahrenheit;
+  return fahrenheit;
 }
 function convertKelvinToCelsius() {
   grados = document.getElementById("grados").value;
   var celsius = parseFloat(grados) - 273.15;
-  document.getElementById("resultado").textContent = celsius;
+  return celsius;
 }
 function kelvinToCelsius() {
   grados = document.getElementById("grados").value;
@@ -37,8 +35,36 @@ function kelvinToCelsius() {
 function celsiusToFahrenheit(celsius) {
   return celsius * 1.8 + 32;
 }
-function todo(){
-  convertCelsiusToFahrenheit();
-  convertCelsiusToKelvin();
-  document.getElementById("resultado-celsius").textContent= 13;
+
+const radios = document.getElementsByName("eleccion");
+
+function seleccion() {
+  let valorSeleccionado;
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      valorSeleccionado = radios[i].id;
+      break;
+    }
+  }
+  return valorSeleccionado;
+}
+
+function todo() {
+  if (seleccion() == "c") {
+    document.getElementById("resultado-celsius").textContent = document.getElementById("grados").value;
+    document.getElementById("resultado-fahrenheit").textContent = convertCelsiusToFahrenheit();
+    document.getElementById("resultado-kelvin").textContent = convertCelsiusToKelvin();
+
+  }
+  if (seleccion() == "f") {
+    document.getElementById("resultado-celsius").textContent = convertFahrenheitToCelsius();
+    document.getElementById("resultado-fahrenheit").textContent = document.getElementById("grados").value;
+    document.getElementById("resultado-kelvin").textContent = convertFahrenheitToKelvin();
+
+  }
+  if (seleccion() == "k") {
+    document.getElementById("resultado-celsius").textContent = convertKelvinToCelsius();
+    document.getElementById("resultado-fahrenheit").textContent = convertKelvinToFahrenheit();
+    document.getElementById("resultado-kelvin").textContent = document.getElementById("grados").value;
+  }
 }
