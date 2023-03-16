d78e67375b61cb6fdd703ed5ca5a33d9922b90b9
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-15 18:56:53 -0600

    casi final

--- Scrip.js
+++ Scrip.js
@@ -1,80 +1,111 @@
-function convertCelsiusToFahrenheit() {
-  grados = document.getElementById("grados").value;
-  var fahrenheit = (grados * 1.8 + 32).toFixed(2);
-  return fahrenheit;
-}
-function convertCelsiusToKelvin() {
-  grados = document.getElementById("grados").value;
-  var kelvin = (parseFloat(grados) + 273.15).toFixed(2);
-  return kelvin;
-}
-function convertFahrenheitToCelsius() {
-  grados = document.getElementById("grados").value;
-  var celsius = ((grados - 32) / 1.8).toFixed(2);
-  return celsius;
-}
-function convertFahrenheitToKelvin() {
-  var celsius = convertFahrenheitToCelsius();
-  var kelvin = (parseFloat(celsius) + 273.15).toFixed(2);
-  return kelvin;
-}
-function convertKelvinToFahrenheit() {
-  var celsius = kelvinToCelsius();
-  var fahrenheit = celsiusToFahrenheit(celsius).toFixed(2);
-  return fahrenheit;
-}
-function convertKelvinToCelsius() {
-  grados = document.getElementById("grados").value;
-  var celsius = (parseFloat(grados) - 273.15).toFixed(2);
-  return celsius;
-}
-function kelvinToCelsius() {
-  grados = document.getElementById("grados").value;
-  return parseFloat(grados) - 273.15;
-}
-function celsiusToFahrenheit(celsius) {
-  return celsius * 1.8 + 32;
-}
-
-const radios = document.getElementsByName("eleccion");
-function seleccion() {
-  let valorSeleccionado;
-  for (let i = 0; i < radios.length; i++) {
-    if (radios[i].checked) {
-      valorSeleccionado = radios[i].id;
-      break;
+function convertCelsiusToFahrenheit(grados) {
+    //   grados = document.getElementById("grados").value;
+    var fahrenheit = (grados * 1.8 + 32).toFixed(2);
+    return fahrenheit;
+  }
+  function convertCelsiusToKelvin(grados) {
+    //   grados = document.getElementById("grados").value;
+    var kelvin = (parseFloat(grados) + 273.15).toFixed(2);
+    return kelvin;
+  }
+  function convertFahrenheitToCelsius(grados) {
+    //   grados = document.getElementById("grados").value;
+    var celsius = ((grados - 32) / 1.8).toFixed(2);
+    return celsius;
+  }
+  function convertFahrenheitToKelvin(grados) {
+    var celsius = convertFahrenheitToCelsius(grados);
+    var kelvin = (parseFloat(celsius) + 273.15).toFixed(2);
+    return kelvin;
+  }
+  function convertKelvinToFahrenheit(grados) {
+    var celsius = kelvinToCelsius(grados);
+    var fahrenheit = celsiusToFahrenheit(celsius).toFixed(2);
+    return fahrenheit;
+  }
+  function convertKelvinToCelsius(grados) {
+    //   grados = document.getElementById("grados").value;
+    var celsius = (parseFloat(grados) - 273.15).toFixed(2);
+    return celsius;
+  }
+  function kelvinToCelsius(grados) {
+    //   grados = document.getElementById("grados").value;
+    return parseFloat(grados) - 273.15;
+  }
+  function celsiusToFahrenheit(celsius) {
+    return celsius * 1.8 + 32;
+  }
+  
+  const radios = document.getElementsByName("eleccion");
+  function seleccion() {
+    let valorSeleccionado;
+    for (let i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        valorSeleccionado = radios[i].id;
+        break;
+      }
+    }
+    return valorSeleccionado;
+  }
+  
+  function todo() {
+    var grados = asignarControladoresDeEventos();
+  //   document.getElementById("rango").value = grados;
+    if (seleccion() == "c") {
+      document.getElementById("resultado-celsius").textContent = grados;
+      document.getElementById("resultado-fahrenheit").textContent =
+        convertCelsiusToFahrenheit(grados);
+      document.getElementById("resultado-kelvin").textContent =
+        convertCelsiusToKelvin(grados);
+    }
+    if (seleccion() == "f") {
+      document.getElementById("resultado-celsius").textContent =
+        convertFahrenheitToCelsius(grados);
+      document.getElementById("resultado-fahrenheit").textContent = grados;
+      document.getElementById("resultado-kelvin").textContent =
+        convertFahrenheitToKelvin(grados);
+    }
+    if (seleccion() == "k") {
+      document.getElementById("resultado-celsius").textContent =
+        convertKelvinToCelsius(grados);
+      document.getElementById("resultado-fahrenheit").textContent =
+        convertKelvinToFahrenheit(grados);
+      document.getElementById("resultado-kelvin").textContent = grados;
     }
   }
-  return valorSeleccionado;
-}
-
-function todo() {
-  if (seleccion() == "c") {
-    document.getElementById("resultado-celsius").textContent = document.getElementById("grados").value;
-    document.getElementById("resultado-fahrenheit").textContent = convertCelsiusToFahrenheit();
-    document.getElementById("resultado-kelvin").textContent = convertCelsiusToKelvin();
+  function validacionDeEntrada() {
+    var result = document.getElementById(grados).value;
+    if (result === null || result.trim() === "") {
+      document.getElementById("resultado-celsius").textContent = "vacio";
+    } else {
+      document.getElementById("resultado-celsius").textContent = "lleno";
+    }
   }
-  if (seleccion() == "f") {
-    document.getElementById("resultado-celsius").textContent = convertFahrenheitToCelsius();
-    document.getElementById("resultado-fahrenheit").textContent = document.getElementById("grados").value;
-    document.getElementById("resultado-kelvin").textContent = convertFahrenheitToKelvin();
+  function getValue() {
+    todo();
+    var valor = document.getElementById("rango").value;
+    document.getElementById("resultado").textContent = valor;
+    document.getElementById("grados").value = valor;
+    return valor;
   }
-  if (seleccion() == "k") {
-    document.getElementById("resultado-celsius").textContent = convertKelvinToCelsius();
-    document.getElementById("resultado-fahrenheit").textContent = convertKelvinToFahrenheit();
-    document.getElementById("resultado-kelvin").textContent = document.getElementById("grados").value;
+  
+  
+  function asignarControladoresDeEventos() {
+    var ultimoInputModificado;
+    var grados;
+    document.getElementById("rango").onchange = function () {
+      ultimoInputModificado = "rangeInput";
+    };
+  
+    document.getElementById("grados").onchange = function () {
+      ultimoInputModificado = "textInput";
+    };
+  
+    if (ultimoInputModificado === "rangeInput") {
+      grados = document.getElementById("rango").value;
+    } else {
+      grados = document.getElementById("grados").value;
+    }
+    return grados;
   }
-}
-function validacionDeEntrada() {
-  var result = document.getElementById(grados).value;
-  if (result === null || result.trim() === "") {
-    document.getElementById("resultado-celsius").textContent = "vacio";
-  }else{
-    document.getElementById("resultado-celsius").textContent = "lleno";
-  }
-}
-function getValue(){
-var valor = document.getElementById("rango").value;
-  document.getElementById("resultado").textContent = valor;
-  return valor;
-}
+  
